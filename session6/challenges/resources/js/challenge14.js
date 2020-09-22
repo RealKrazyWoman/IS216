f8d317065a4990cf74cc8295a8e4e6643badbead
RealKrazyWoman <RealKrazyWoman@gmail.com>
2020-09-22 21:51:07 +0800

    Session 6 Challenges

---
+++ session6/challenges/resources/js/challenge14.js
@@ -0,0 +1,28 @@
+// KrazyMLBB API v1.0 - Documentation
+// http://krazywoman.com/krazymlbb/
+
+
+// When the webpage loads
+// Display all heroes
+function display_default() {
+
+    // Display all heroes
+    
+    // YOUR CODE GOES HERE
+    
+}
+
+
+// Given a hero_class (tank, fighter, mage, asassin, marksman, support, all)
+function show_heroes(hero_class) {
+
+    // if 'tank'
+    // only display 'tank' heroes
+
+    // if 'all'
+    // display ALL heroes
+
+    // YOUR CODE GOES HERE
+
+
+}
